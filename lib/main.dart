e075cf51522af6a3e5fe5c50a41ac847726a2cb5
m-alajmi-eng <rooffee.sa@icloud.com>
2026-06-23 15:11:14 +0300

    TASK 1.4: Firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
